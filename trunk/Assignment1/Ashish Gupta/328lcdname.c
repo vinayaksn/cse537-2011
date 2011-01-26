23f9bdbf7c25b2fd5da3d78bee1544b2fe3dc49c
ashishgupta.061990@gmail.com <ashishgupta.061990@gmail.com@219ac628-5506-f32d-1171-a5a0be373951>
2011-01-26 17:30:00 +0000

    final committed code

--- trunk/Assignment1/Ashish Gupta/328lcdname.c
+++ trunk/Assignment1/Ashish Gupta/328lcdname.c
@@ -17,22 +17,20 @@ int main()
    _delay_ms(100);
    lcd_clrscr();
    _delay_ms(100);
-
-   //lcd_command( (1<<LCD_MOVE) | (1<<LCD_MOVE_RIGHT) );
    _delay_ms(10);
       
    while(1)
    {      
-      lcd_clrscr();
-	  _delay_ms(100);	  
-      lcd_puts("Ashish Gupta");	  
-	  _delay_ms(100);
-	for(int i=0;i<16;i++)
-	{lcd_command(LCD_MOVE_DISP_RIGHT);
-	_delay_ms(500); 
-   }
+    lcd_clrscr();
+	_delay_ms(100);	  
+    lcd_puts("Ashish Gupta");	  
+	_delay_ms(100);
+		for(int i=0;i<16;i++)
+		{
+			lcd_command(LCD_MOVE_DISP_RIGHT);
+			_delay_ms(500); 
+		}
    }
    return 0;
 }
-
 
