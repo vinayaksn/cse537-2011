e984c0072a1cf6ffeffb169628f60b12891217d5
pratyush08040@iiitd.ac.in <pratyush08040@iiitd.ac.in@219ac628-5506-f32d-1171-a5a0be373951>
2011-01-26 11:07:07 +0000

    Final Submission.

---
+++ trunk/Assignment1/Pratyush Tripathi/EsAss1.c
@@ -0,0 +1,75 @@
+
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+#include "lcd.h"
+#include "lcd.c"
+#include "delay_x.h"
+
+
+
+int main()
+{
+
+   lcd_init(LCD_DISP_ON);
+   _delay_ms(100);
+   lcd_clrscr();
+   _delay_ms(100);
+
+   lcd_command( (1<<LCD_MOVE) | (1<<LCD_MOVE_RIGHT) );
+   _delay_ms(10);
+      
+   while(1)
+   {      
+      lcd_clrscr();
+          _delay_ms(100);         
+      lcd_puts("Pratyush");   
+          _delay_ms(100);
+        for(int i=0;i<16;i++)
+        {
+		lcd_command(LCD_MOVE_DISP_RIGHT);
+        _delay_ms(500); 
+        }
+   }
+   return 0;
+}
+
+
+
+
+
+
+
+/*
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+#include "lcd.h"
+#include "lcd.c"
+#include "delay_x.h"
+
+
+
+int main()
+{
+
+   lcd_init(LCD_DISP_ON);
+   _delay_ms(1000);
+   lcd_clrscr();
+   _delay_ms(100);
+   _delay_ms(50);
+      
+   while(1)
+   {      
+      lcd_clrscr();
+	  //_delay_ms(1000);	  
+      lcd_puts("PRATYUSH TRIPATHI");	  
+	  //_delay_ms(1000);
+   }
+   return 0;
+}
+*/
